bcb8d16fb7464f3996b4d807bcf77756de9f4d36
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-22 19:47:20 +0530

    added use memo for better performance

--- frontend/src/components/UseMemo.jsx
+++ frontend/src/components/UseMemo.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function UseMemo() {
   const [counter, setCounter] = useState(0);
   const [inputValue, setInputValue] = useState(1);
 
-  let count = 0;
-  for (let i = 1; i < inputValue; i++) {
-    count = count + i;
-  }
+
+  let count = useMemo(()=>{
+    let count = 0;
+    for (let i = 1; i < inputValue; i++) {
+      count = count + i;
+    }
+    return count;
+  },[inputValue])
 
   return (
     <div>
