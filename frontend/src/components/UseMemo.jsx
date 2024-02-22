35b426e1077c922d80fc122fe908e09a58f9a863
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-22 19:16:10 +0530

    changes in input

--- frontend/src/components/UseMemo.jsx
+++ frontend/src/components/UseMemo.jsx
@@ -1,23 +1,22 @@
-
 import { useState } from "react";
 
-export default function UseMemo(){
+export default function UseMemo() {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
 
-    const [counter,setCounter] = useState(0)
-    const [inputValue,setInputValue] = useState(1)
+  let count = 0;
+  for (let i = 1; i < inputValue; i++) {
+    count = count + i;
+  }
 
-    let count = 0;
-    for(let i=1; i<inputValue; i++){
-        count = count+i;
-    }
-
-
-    return(
-        <div>
-        <input type="text" placeholder="input area"/>
-        <br/>
-        <br/>
-        <button>counter {counter}</button>
-        </div>
-    )
+  return (
+    <div>
+      <input type="text" placeholder="input area" onChange={function(event){
+        setInputValue(event.target.value)
+      }}/>
+      <br />
+      <br />
+      <button>counter {counter}</button>
+    </div>
+  );
 }
