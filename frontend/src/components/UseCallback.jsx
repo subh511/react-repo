34f4762a4fa5f5dec9c15240ee94123150cf7b2b
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-23 12:26:57 +0530

    useCallback & custom hooks

--- frontend/src/components/UseCallback.jsx
+++ frontend/src/components/UseCallback.jsx
@@ -1,14 +1,33 @@
-import { useState,memo,useCallback } from "react"
+import { useState, memo, useCallback } from "react";
 
+export default function UseCallback() {
+  const [count, setcount] = useState(0);
 
-export default function UseCallback(){
+  const inputFunction = useCallback(()=>{
+    console.log("hi there")
+  },[])
 
-    const [count,setcount] = useState(0);
-
-    return(
-        <div>
-        <h1>{count}</h1>
-        <button onClick={()=>{setcount(count+1)}}>increaseCount</button>
-        </div>
-    )
+  return (
+    <div>
+      <ButtonComponent inputFunction={inputFunction}/>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setcount(count + 1);
+        }}
+      >
+        increaseCount
+      </button>
+    </div>
+  );
 }
+
+const ButtonComponent = memo(function({inputFunction}){
+
+    console.log("child render")
+    return (
+        <div>
+          <button>Button Clicked</button>
+        </div>
+      );
+})
